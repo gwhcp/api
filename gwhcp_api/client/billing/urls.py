08733a50afe9cdf07e928632f7f574b85f3fbfd4
Sammie S. Taunton <diemuzi@gmail.com>
2021-09-17 07:42:41 -0500

    Removed store/retail

--- gwhcp_api/client/billing/urls.py
+++ gwhcp_api/client/billing/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from store.retail import views
+from client.billing import views
 
 urlpatterns = [
     path(
         'create',
         views.Create.as_view(),
         name='create'
+    ),
+
+    path(
+        'search',
+        views.Search.as_view(),
+        name='search'
     )
 ]
--- gwhcp_api/store/retail/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class Config(AppConfig):
-    label = 'store_retail'
-
-    name = 'store.retail'
-
-    verbose_name = 'Store Retail'
